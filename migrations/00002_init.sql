ca9614236db1b9bbd343faf4d72cf2571e33066e
malijie <malijie@360.cn>
2025-09-23 15:00:28 +0800

    init sql

--- migrations/00002_init.sql
+++ migrations/00002_init.sql
@@ -13,10 +13,11 @@ INSERT INTO `role_permissions` (`role_code`, `permission_code`) VALUES
  ('sup_admin', 'captcha_set');
 
 INSERT INTO `permissions` (`code`, `name`, `description`, `path`, `module`, `global_flag`) VALUES
+('user_logout', '退出', '', 'admin/v1/user/logout', 'user', 1),
+('user_changePwd', '修改密码', '', 'admin/v1/user/changePwd', 'user', 1),
 ('captcha_get', '查询验证码', '', 'admin/v1/setting/captcha-switch', 'server', 0),
 ('captcha_set', '设置验证码', '', 'admin/v1/setting/captcha-switch', 'server', 0),
-('user_logout', '退出', '', 'admin/v1/user/logout', 'user', 1),
-('user_changePwd', '修改密码', '', 'admin/v1/user/changePwd', 'user', 1);
+('role_active', '活跃角色', '', 'admin/v1/role/active', 'role', 0);
 
 INSERT INTO server_setting (id, name, value) VALUES
 ('1', 'captcha_switch', '{"admin":0, "web":1}');
