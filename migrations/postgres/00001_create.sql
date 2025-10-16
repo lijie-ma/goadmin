0b7a9cbdcfee51c306f3b05858de6c8c7c619663
malijie <malijie@360.cn>
2025-10-16 17:13:05 +0800

    init db adapter

---
+++ migrations/postgres/00001_create.sql
@@ -0,0 +1,76 @@
+-- +goose Up
+-- SQL in this section is executed when the migration is applied.
+
+-- 创建 permissions 表
+CREATE TABLE permissions (
+  id SERIAL PRIMARY KEY,
+  ctime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  mtime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  code VARCHAR(32) NOT NULL DEFAULT '',
+  name VARCHAR(50) NOT NULL DEFAULT '',
+  description VARCHAR(200),
+  path VARCHAR(200) NOT NULL DEFAULT '',
+  module VARCHAR(50) NOT NULL DEFAULT '',
+  global_flag SMALLINT DEFAULT 0, -- 1:全局权限,0:局部权限
+  UNIQUE (code),
+  UNIQUE (name)
+);
+
+-- 创建 roles 表
+CREATE TABLE roles (
+  id SERIAL PRIMARY KEY,
+  ctime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  mtime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  code VARCHAR(32) NOT NULL DEFAULT '',
+  name VARCHAR(50) NOT NULL DEFAULT '',
+  description VARCHAR(200),
+  status INT DEFAULT 1, -- 1:active,0:inactive
+  system_flag SMALLINT DEFAULT 0, -- 1:系统内置,0:用户自定义
+  UNIQUE (code),
+  UNIQUE (name)
+);
+
+-- 创建 users 表
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  ctime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  mtime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  username VARCHAR(50) NOT NULL DEFAULT '',
+  password VARCHAR(100) NOT NULL DEFAULT '',
+  email VARCHAR(100),
+  role_code VARCHAR(32) NOT NULL DEFAULT '',
+  status INT DEFAULT 1, -- 0:inactive,1:active,2:locked,3:deleted
+  UNIQUE (username),
+  UNIQUE (email)
+);
+
+-- 创建 role_permissions 表
+CREATE TABLE role_permissions (
+  id SERIAL PRIMARY KEY,
+  ctime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  mtime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  role_code VARCHAR(32) NOT NULL DEFAULT '',
+  permission_code VARCHAR(32) NOT NULL DEFAULT '',
+  UNIQUE (role_code, permission_code)
+);
+
+-- 创建 server_setting 表 (10000以内为系统内置)
+CREATE TABLE server_setting (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(64) NOT NULL DEFAULT '',
+  value VARCHAR(1024) NOT NULL DEFAULT '',
+  mtime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  ctime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  UNIQUE (name)
+);
+
+COMMENT ON TABLE server_setting IS '服务端配置';
+COMMENT ON COLUMN server_setting.id IS '10000以内为系统内置配置';
+
+-- +goose Down
+-- SQL in this section is executed when the migration is rolled back.
+DROP TABLE IF EXISTS server_setting;
+DROP TABLE IF EXISTS role_permissions;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS permissions;
---
+++ migrations/postgres/00002_init.sql
@@ -0,0 +1,53 @@
+-- +goose Up
+-- SQL in this section is executed when the migration is applied.
+
+--  admin 123456
+INSERT INTO users (id, username, password, email, status, role_code) VALUES
+(1, 'admin', '$2y$10$oPdNH9rQsliggddN1lifkeIhHFGVSUjKnfT.EcyPrPMZiTRWnBdHm', '', 1, 'sup_admin');
+
+INSERT INTO roles (code, name, description, status, system_flag) VALUES
+('sup_admin', '超级管理员', '超级管理员', 1, 1);
+
+INSERT INTO role_permissions (role_code, permission_code) VALUES
+ ('sup_admin', 'captcha_get'),
+ ('sup_admin', 'captcha_set'),
+ ('sup_admin', 'server_set'),
+ ('sup_admin', 'server_get'),
+ ('sup_admin', 'server_batch_get'),
+ ('sup_admin', 'role_active'),
+ ('sup_admin', 'role_list'),
+ ('sup_admin', 'role_info'),
+ ('sup_admin', 'role_create'),
+ ('sup_admin', 'role_update'),
+ ('sup_admin', 'role_delete'),
+ ('sup_admin', 'role_perm_set'),
+ ('sup_admin', 'role_perm_info');
+
+INSERT INTO permissions (code, name, description, path, module, global_flag) VALUES
+('user_logout',    '退出',         '', 'admin/v1/user/logout',              'user',   1),
+('user_changePwd', '修改密码',     '', 'admin/v1/user/changePwd',           'user',   1),
+('captcha_get',    '查询验证码',   '', 'admin/v1/setting/captcha-switch',   'server', 0),
+('captcha_set',    '设置验证码',   '', 'admin/v1/setting/captcha-switch',   'server', 0),
+('server_set',    '系统设置',   '', 'admin/v1/setting/set',   'server', 0),
+('server_get',    '系统设置查询',   '', 'admin/v1/setting/get',   'server', 0),
+('server_batch_get','系统设置批量查询',   '', 'admin/v1/setting/batch',   'server', 0),
+('role_active',    '活跃角色',     '', 'admin/v1/role/active',              'role',   0),
+('role_list',      '角色列表',     '', 'admin/v1/role',                     'role',   0),
+('role_info',      '角色详情',     '', 'admin/v1/role/get',                 'role',   0),
+('role_create',    '角色创建',     '', 'admin/v1/role/create',              'role',   0),
+('role_update',    '角色更新',     '', 'admin/v1/role/update',              'role',   0),
+('role_delete',    '角色删除',     '', 'admin/v1/role/delete',              'role',   0),
+('role_perm_set',  '角色权限设置', '', 'admin/v1/role/permissions/assign',  'role',   0),
+('role_perm_info', '角色权限查看', '', 'admin/v1/role/permissions/get',     'role',   0);
+
+
+INSERT INTO server_setting (id, name, value) VALUES
+(1, 'captcha_switch', '{"admin":0, "web":1}');
+
+-- +goose Down
+-- SQL in this section is executed when the migration is rolled back.
+delete from roles where code = 'sup_admin';
+delete from users where username = 'admin';
+delete from permissions where id>0;
+delete from role_permissions where id>0;
+delete from server_setting where name in ('captcha_switch');
