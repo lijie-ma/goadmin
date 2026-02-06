d1f065d4d72bf45b93c6811ca683ab620a25c503
malijie <malijie@360.cn>
2026-02-06 17:31:38 +0800

    fix: position model add json tag

--- internal/model/position/position.go
+++ internal/model/position/position.go
@@ -6,13 +6,13 @@ import (
 
 type Position struct {
 	schema.BaseModel
-	City       string  `gorm:"column:city;type:varchar(64);not null;default:'';index:idx_city;comment:城市名称"`
-	Location   string  `gorm:"column:location;type:varchar(128);not null;default:'';index:idx_location;comment:详细位置（如街道/建筑）"`
-	Longitude  float64 `gorm:"column:longitude;type:numeric(10,6);not null;comment:经度"`
-	Latitude   float64 `gorm:"column:latitude;type:numeric(10,6);not null;comment:纬度"`
-	CustomName string  `gorm:"column:custom_name;type:varchar(128);comment:自定义名称"`
-	CreatorID  int     `gorm:"column:creator_id;type:int;default:0;comment:创建人ID"`
-	Creator    string  `gorm:"column:creator;type:varchar(64);not null;default:'';comment:创建人"`
+	City       string  `gorm:"column:city;type:varchar(64);not null;default:'';index:idx_city;comment:城市名称" json:"city"`
+	Location   string  `gorm:"column:location;type:varchar(128);not null;default:'';index:idx_location;comment:详细位置（如街道/建筑）" json:"location"`
+	Longitude  float64 `gorm:"column:longitude;type:numeric(10,6);not null;comment:经度" json:"longitude"`
+	Latitude   float64 `gorm:"column:latitude;type:numeric(10,6);not null;comment:纬度" json:"latitude"`
+	CustomName string  `gorm:"column:custom_name;type:varchar(128);comment:自定义名称" json:"custom_name"`
+	CreatorID  int     `gorm:"column:creator_id;type:int;default:0;comment:创建人ID" json:"creator_id"`
+	Creator    string  `gorm:"column:creator;type:varchar(64);not null;default:'';comment:创建人" json:"creator"`
 }
 
 // TableName 指定表名
